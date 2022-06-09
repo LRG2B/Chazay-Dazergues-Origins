9ac6b0903844dce0d16a7ff8b29c7cab632ba39a
MisterBulle <101103057+MisterBulle@users.noreply.github.com>
2022-06-09 12:14:32 +0200

    Cycle Jour-Nuit
    ATTENTION !
    Il se peut qu'il faudra installer des packages et les paramétrer !
    Pour l'activer sur la scène actuelle :+1:
    Edit -> Render Pipeline -> URP -> 2D Renderer -> Upgrade 2D -> Upgrade Scene

--- Chazay-Dazergues/Assets/Scripts/game_meca/DayAndNightCycle.cs
+++ Chazay-Dazergues/Assets/Scripts/game_meca/DayAndNightCycle.cs
@@ -13,29 +13,32 @@ public class DayAndNightCycle : MonoBehaviour
     //Getters (Days prend les valeurs de days)
     public int Days => days;
 
-    public float time = 50;
+    private float time = 1;
     private bool canChangeDay = true;
 
     // Update is called once per frame
     private void Update()
     {
-        if (time > 500)
+        if (time > 100)
         {
             time = 0;
         }
-
-        if ((int) time == 500 && canChangeDay)
+        //500
+        if ((int) time == 100 && canChangeDay)
         {
             canChangeDay = false;
             days++;
         }
-
-        if ((int) time == 255)
+        //255
+        if ((int) time == 51)
         {
             canChangeDay = true;
         }
 
         time += Time.deltaTime;
-        light.GetComponent<Light2D>().color = lightColor.Evaluate(time + 0.002f);
+        light.GetComponent<Light2D>().color = lightColor.Evaluate(time * 0.02f);
+
+        //Pour 500 secondes
+        //light.GetComponent<Light2D>().color = lightColor.Evaluate(time * 0.002f);
     }
 }
